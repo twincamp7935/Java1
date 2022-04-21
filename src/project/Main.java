a9a6d8ea0ffa79589ccf066c4ff90b6c68324c8f
runneradmin <runneradmin@fv-az153-685.ealj4gunuyuunol34ucrstyjbd.cx.internal.cloudapp.net>
2022-04-21 22:56:34 +0900

    2022-04-21T22:56:34.067+09:00

--- src/project/Main.java
+++ src/project/Main.java
@@ -8,6 +8,10 @@ public class Main {
 		System.out.println("HelloWorld");
 		// START
 
+		Moavknwq moavknwq = new Moavknwq();
+		moavknwq.speak();
+
+
 		Gewrcwnalr gewrcwnalr = new Gewrcwnalr();
 		gewrcwnalr.speak();
 
---
+++ src/project/Moavknwq.java
@@ -0,0 +1,13 @@
+package project;
+
+public class Moavknwq {
+	String message = "Moavknwq";
+	
+	public Moavknwq() {
+		super();
+	}
+	
+	public void speak() {
+		System.out.println(message);
+	}
+}
