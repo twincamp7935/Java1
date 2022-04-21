a0f595fc049fc116523d08a62880bad7e5f7b601
runneradmin <runneradmin@fv-az165-439.nmkrvjyc3ehubaicle1ztxvo2e.gx.internal.cloudapp.net>
2022-04-22 04:00:34 +0900

    2022-04-22T04:00:34.315+09:00

--- src/project/Main.java
+++ src/project/Main.java
@@ -8,6 +8,10 @@ public class Main {
 		System.out.println("HelloWorld");
 		// START
 
+		Vnuxtl vnuxtl = new Vnuxtl();
+		vnuxtl.speak();
+
+
 		Moavknwq moavknwq = new Moavknwq();
 		moavknwq.speak();
 
---
+++ src/project/Vnuxtl.java
@@ -0,0 +1,13 @@
+package project;
+
+public class Vnuxtl {
+	String message = "Vnuxtl";
+	
+	public Vnuxtl() {
+		super();
+	}
+	
+	public void speak() {
+		System.out.println(message);
+	}
+}
