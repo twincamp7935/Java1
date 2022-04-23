9bf5a9f9be15e602de68640410d2818952c72c86
runneradmin <runneradmin@fv-az68-480.kwvurbkbxwfujn0qws0grc4tpg.bx.internal.cloudapp.net>
2022-04-24 00:13:26 +0900

    2022-04-24T00:13:26.384+09:00

---
+++ src/project/Gvmdjupbai.java
@@ -0,0 +1,13 @@
+package project;
+
+public class Gvmdjupbai {
+	String message = "Gvmdjupbai";
+	
+	public Gvmdjupbai() {
+		super();
+	}
+	
+	public void speak() {
+		System.out.println(message);
+	}
+}
--- src/project/Main.java
+++ src/project/Main.java
@@ -8,6 +8,10 @@ public class Main {
 		System.out.println("HelloWorld");
 		// START
 
+		Gvmdjupbai gvmdjupbai = new Gvmdjupbai();
+		gvmdjupbai.speak();
+
+
 		Gdpvrvefq gdpvrvefq = new Gdpvrvefq();
 		gdpvrvefq.speak();
 
