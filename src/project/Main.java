0e91d77aba0a0934a538d0ff26576f1a1a5c3ff5
runneradmin <runneradmin@fv-az237-253.biufcjdeo2uevjobbzqu33ctec.bx.internal.cloudapp.net>
2022-04-24 23:28:48 +0900

    2022-04-24T23:28:48.608+09:00

--- src/project/Main.java
+++ src/project/Main.java
@@ -8,6 +8,10 @@ public class Main {
 		System.out.println("HelloWorld");
 		// START
 
+		Xgear xgear = new Xgear();
+		xgear.speak();
+
+
 		Mijdqwarip mijdqwarip = new Mijdqwarip();
 		mijdqwarip.speak();
 
---
+++ src/project/Xgear.java
@@ -0,0 +1,13 @@
+package project;
+
+public class Xgear {
+	String message = "Xgear";
+	
+	public Xgear() {
+		super();
+	}
+	
+	public void speak() {
+		System.out.println(message);
+	}
+}
