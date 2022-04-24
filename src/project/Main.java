1bfcecf61dca7559d086de220785d17f67c9771e
runneradmin <runneradmin@fv-az153-685.ealj4gunuyuunol34ucrstyjbd.cx.internal.cloudapp.net>
2022-04-24 18:25:13 +0900

    2022-04-24T18:25:13.815+09:00

--- src/project/Main.java
+++ src/project/Main.java
@@ -8,6 +8,10 @@ public class Main {
 		System.out.println("HelloWorld");
 		// START
 
+		Mijdqwarip mijdqwarip = new Mijdqwarip();
+		mijdqwarip.speak();
+
+
 		Xhdbdlxiac xhdbdlxiac = new Xhdbdlxiac();
 		xhdbdlxiac.speak();
 
---
+++ src/project/Mijdqwarip.java
@@ -0,0 +1,13 @@
+package project;
+
+public class Mijdqwarip {
+	String message = "Mijdqwarip";
+	
+	public Mijdqwarip() {
+		super();
+	}
+	
+	public void speak() {
+		System.out.println(message);
+	}
+}
