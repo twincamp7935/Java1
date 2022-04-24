1588286050cd61c1ad0e1bd9b14cfeec6b1151ab
runneradmin <runneradmin@fv-az237-253.biufcjdeo2uevjobbzqu33ctec.bx.internal.cloudapp.net>
2022-04-25 04:29:00 +0900

    2022-04-25T04:29:00.252+09:00

---
+++ src/project/Iothmediie.java
@@ -0,0 +1,13 @@
+package project;
+
+public class Iothmediie {
+	String message = "Iothmediie";
+	
+	public Iothmediie() {
+		super();
+	}
+	
+	public void speak() {
+		System.out.println(message);
+	}
+}
--- src/project/Main.java
+++ src/project/Main.java
@@ -8,6 +8,10 @@ public class Main {
 		System.out.println("HelloWorld");
 		// START
 
+		Iothmediie iothmediie = new Iothmediie();
+		iothmediie.speak();
+
+
 		Xgear xgear = new Xgear();
 		xgear.speak();
 
