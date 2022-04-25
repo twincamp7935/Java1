8e2c12748a1bb28a5b14d37eb7852000c556255a
runneradmin <runneradmin@fv-az153-685.ealj4gunuyuunol34ucrstyjbd.cx.internal.cloudapp.net>
2022-04-25 16:04:22 +0900

    2022-04-25T16:04:22.766+09:00

--- src/project/Main.java
+++ src/project/Main.java
@@ -8,6 +8,10 @@ public class Main {
 		System.out.println("HelloWorld");
 		// START
 
+		Njsqyowrob njsqyowrob = new Njsqyowrob();
+		njsqyowrob.speak();
+
+
 		Jgrdcivnnp jgrdcivnnp = new Jgrdcivnnp();
 		jgrdcivnnp.speak();
 
---
+++ src/project/Njsqyowrob.java
@@ -0,0 +1,13 @@
+package project;
+
+public class Njsqyowrob {
+	String message = "Njsqyowrob";
+	
+	public Njsqyowrob() {
+		super();
+	}
+	
+	public void speak() {
+		System.out.println(message);
+	}
+}
