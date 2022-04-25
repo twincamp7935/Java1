664c9f9ecad684a02684fcb7458d5da029d9bf15
runneradmin <runneradmin@fv-az153-685.ealj4gunuyuunol34ucrstyjbd.cx.internal.cloudapp.net>
2022-04-26 08:00:55 +0900

    2022-04-26T08:00:55.946+09:00

--- src/project/Main.java
+++ src/project/Main.java
@@ -8,6 +8,10 @@ public class Main {
 		System.out.println("HelloWorld");
 		// START
 
+		Usdhogyiy usdhogyiy = new Usdhogyiy();
+		usdhogyiy.speak();
+
+
 		Mdypgw mdypgw = new Mdypgw();
 		mdypgw.speak();
 
---
+++ src/project/Usdhogyiy.java
@@ -0,0 +1,13 @@
+package project;
+
+public class Usdhogyiy {
+	String message = "Usdhogyiy";
+	
+	public Usdhogyiy() {
+		super();
+	}
+	
+	public void speak() {
+		System.out.println(message);
+	}
+}
