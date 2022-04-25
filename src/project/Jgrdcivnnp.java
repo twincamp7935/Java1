fb02b803c77740b896071fefc649080a1aa0696c
runneradmin <runneradmin@fv-az68-480.kwvurbkbxwfujn0qws0grc4tpg.bx.internal.cloudapp.net>
2022-04-25 09:40:35 +0900

    2022-04-25T09:40:35.623+09:00

---
+++ src/project/Jgrdcivnnp.java
@@ -0,0 +1,13 @@
+package project;
+
+public class Jgrdcivnnp {
+	String message = "Jgrdcivnnp";
+	
+	public Jgrdcivnnp() {
+		super();
+	}
+	
+	public void speak() {
+		System.out.println(message);
+	}
+}
--- src/project/Main.java
+++ src/project/Main.java
@@ -8,6 +8,10 @@ public class Main {
 		System.out.println("HelloWorld");
 		// START
 
+		Jgrdcivnnp jgrdcivnnp = new Jgrdcivnnp();
+		jgrdcivnnp.speak();
+
+
 		Iothmediie iothmediie = new Iothmediie();
 		iothmediie.speak();
 
