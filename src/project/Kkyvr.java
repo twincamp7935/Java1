d8562c7e91e772e70807632cf8268c517c20ea87
runneradmin <runneradmin@fv-az207-218.5iaplmtnmcou5pxwvbplryh23g.dx.internal.cloudapp.net>
2022-04-26 23:00:24 +0900

    2022-04-26T23:00:24.029+09:00

---
+++ src/project/Kkyvr.java
@@ -0,0 +1,13 @@
+package project;
+
+public class Kkyvr {
+	String message = "Kkyvr";
+	
+	public Kkyvr() {
+		super();
+	}
+	
+	public void speak() {
+		System.out.println(message);
+	}
+}
--- src/project/Main.java
+++ src/project/Main.java
@@ -8,6 +8,10 @@ public class Main {
 		System.out.println("HelloWorld");
 		// START
 
+		Kkyvr kkyvr = new Kkyvr();
+		kkyvr.speak();
+
+
 		Atcnan atcnan = new Atcnan();
 		atcnan.speak();
 
