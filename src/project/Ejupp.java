a4c5c3ad6785caa77921e0081bf51a5af7768733
runneradmin <runneradmin@fv-az237-253.biufcjdeo2uevjobbzqu33ctec.bx.internal.cloudapp.net>
2022-04-26 13:26:42 +0900

    2022-04-26T13:26:42.127+09:00

---
+++ src/project/Ejupp.java
@@ -0,0 +1,13 @@
+package project;
+
+public class Ejupp {
+	String message = "Ejupp";
+	
+	public Ejupp() {
+		super();
+	}
+	
+	public void speak() {
+		System.out.println(message);
+	}
+}
--- src/project/Main.java
+++ src/project/Main.java
@@ -8,6 +8,10 @@ public class Main {
 		System.out.println("HelloWorld");
 		// START
 
+		Ejupp ejupp = new Ejupp();
+		ejupp.speak();
+
+
 		Usdhogyiy usdhogyiy = new Usdhogyiy();
 		usdhogyiy.speak();
 
