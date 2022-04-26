c0deb8fa847f19f1c1c4df6e298f6f4f80152359
runneradmin <runneradmin@fv-az237-253.biufcjdeo2uevjobbzqu33ctec.bx.internal.cloudapp.net>
2022-04-26 17:35:39 +0900

    2022-04-26T17:35:38.981+09:00

---
+++ src/project/Atcnan.java
@@ -0,0 +1,13 @@
+package project;
+
+public class Atcnan {
+	String message = "Atcnan";
+	
+	public Atcnan() {
+		super();
+	}
+	
+	public void speak() {
+		System.out.println(message);
+	}
+}
--- src/project/Main.java
+++ src/project/Main.java
@@ -8,6 +8,10 @@ public class Main {
 		System.out.println("HelloWorld");
 		// START
 
+		Atcnan atcnan = new Atcnan();
+		atcnan.speak();
+
+
 		Ejupp ejupp = new Ejupp();
 		ejupp.speak();
 
