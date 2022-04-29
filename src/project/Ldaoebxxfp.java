bd7de4431d5ab9d5a08ff5870b40f68617f65c75
runneradmin <runneradmin@fv-az207-300.5iaplmtnmcou5pxwvbplryh23g.dx.internal.cloudapp.net>
2022-04-29 11:05:59 +0900

    2022-04-29T11:05:59.257+09:00

---
+++ src/project/Ldaoebxxfp.java
@@ -0,0 +1,13 @@
+package project;
+
+public class Ldaoebxxfp {
+	String message = "Ldaoebxxfp";
+	
+	public Ldaoebxxfp() {
+		super();
+	}
+	
+	public void speak() {
+		System.out.println(message);
+	}
+}
--- src/project/Main.java
+++ src/project/Main.java
@@ -8,6 +8,10 @@ public class Main {
 		System.out.println("HelloWorld");
 		// START
 
+		Ldaoebxxfp ldaoebxxfp = new Ldaoebxxfp();
+		ldaoebxxfp.speak();
+
+
 		Yrwqfauev yrwqfauev = new Yrwqfauev();
 		yrwqfauev.speak();
 
