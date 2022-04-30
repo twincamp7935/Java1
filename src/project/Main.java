a2c913415f8f926f5d5e8b736fec7c251db67bc5
runneradmin <runneradmin@fv-az207-300.5iaplmtnmcou5pxwvbplryh23g.dx.internal.cloudapp.net>
2022-05-01 08:55:14 +0900

    2022-05-01T08:55:14.739+09:00

--- src/project/Main.java
+++ src/project/Main.java
@@ -8,6 +8,10 @@ public class Main {
 		System.out.println("HelloWorld");
 		// START
 
+		Prxptuvl prxptuvl = new Prxptuvl();
+		prxptuvl.speak();
+
+
 		Shgxqx shgxqx = new Shgxqx();
 		shgxqx.speak();
 
---
+++ src/project/Prxptuvl.java
@@ -0,0 +1,13 @@
+package project;
+
+public class Prxptuvl {
+	String message = "Prxptuvl";
+	
+	public Prxptuvl() {
+		super();
+	}
+	
+	public void speak() {
+		System.out.println(message);
+	}
+}
