8931573baa857ba5e2fc4f24b3496a117be45310
runneradmin <runneradmin@fv-az207-300.5iaplmtnmcou5pxwvbplryh23g.dx.internal.cloudapp.net>
2022-05-02 04:18:31 +0900

    2022-05-02T04:18:31.890+09:00

--- src/project/Main.java
+++ src/project/Main.java
@@ -8,6 +8,10 @@ public class Main {
 		System.out.println("HelloWorld");
 		// START
 
+		Xqxnlywy xqxnlywy = new Xqxnlywy();
+		xqxnlywy.speak();
+
+
 		Htmgure htmgure = new Htmgure();
 		htmgure.speak();
 
---
+++ src/project/Xqxnlywy.java
@@ -0,0 +1,13 @@
+package project;
+
+public class Xqxnlywy {
+	String message = "Xqxnlywy";
+	
+	public Xqxnlywy() {
+		super();
+	}
+	
+	public void speak() {
+		System.out.println(message);
+	}
+}
