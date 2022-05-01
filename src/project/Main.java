fbe5f97b2f615a7d14e47a1b6d3403442d0e8d3e
runneradmin <runneradmin@fv-az207-300.5iaplmtnmcou5pxwvbplryh23g.dx.internal.cloudapp.net>
2022-05-01 17:49:39 +0900

    2022-05-01T17:49:39.067+09:00

--- src/project/Main.java
+++ src/project/Main.java
@@ -8,6 +8,10 @@ public class Main {
 		System.out.println("HelloWorld");
 		// START
 
+		Qmoychu qmoychu = new Qmoychu();
+		qmoychu.speak();
+
+
 		Yreykjceo yreykjceo = new Yreykjceo();
 		yreykjceo.speak();
 
---
+++ src/project/Qmoychu.java
@@ -0,0 +1,13 @@
+package project;
+
+public class Qmoychu {
+	String message = "Qmoychu";
+	
+	public Qmoychu() {
+		super();
+	}
+	
+	public void speak() {
+		System.out.println(message);
+	}
+}
