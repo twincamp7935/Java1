d8391a48691102826d47832e9ef0da44d067a10d
runneradmin <runneradmin@fv-az207-300.5iaplmtnmcou5pxwvbplryh23g.dx.internal.cloudapp.net>
2022-05-01 16:31:40 +0900

    2022-05-01T16:31:40.112+09:00

--- src/project/Main.java
+++ src/project/Main.java
@@ -8,6 +8,10 @@ public class Main {
 		System.out.println("HelloWorld");
 		// START
 
+		Yreykjceo yreykjceo = new Yreykjceo();
+		yreykjceo.speak();
+
+
 		Prxptuvl prxptuvl = new Prxptuvl();
 		prxptuvl.speak();
 
---
+++ src/project/Yreykjceo.java
@@ -0,0 +1,13 @@
+package project;
+
+public class Yreykjceo {
+	String message = "Yreykjceo";
+	
+	public Yreykjceo() {
+		super();
+	}
+	
+	public void speak() {
+		System.out.println(message);
+	}
+}
