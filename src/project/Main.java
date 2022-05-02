eb048d324cae51c08b09a8353683e7a0a6d988a1
runneradmin <runneradmin@fv-az207-300.5iaplmtnmcou5pxwvbplryh23g.dx.internal.cloudapp.net>
2022-05-02 18:32:13 +0900

    2022-05-02T18:32:13.922+09:00

--- src/project/Main.java
+++ src/project/Main.java
@@ -8,6 +8,10 @@ public class Main {
 		System.out.println("HelloWorld");
 		// START
 
+		Qumxmld qumxmld = new Qumxmld();
+		qumxmld.speak();
+
+
 		Sjsbfkenkf sjsbfkenkf = new Sjsbfkenkf();
 		sjsbfkenkf.speak();
 
---
+++ src/project/Qumxmld.java
@@ -0,0 +1,13 @@
+package project;
+
+public class Qumxmld {
+	String message = "Qumxmld";
+	
+	public Qumxmld() {
+		super();
+	}
+	
+	public void speak() {
+		System.out.println(message);
+	}
+}
