3bf772b2e6383fbeb520bac8e7158178358ae1a8
runneradmin <runneradmin@fv-az207-137.5iaplmtnmcou5pxwvbplryh23g.dx.internal.cloudapp.net>
2022-05-05 06:55:43 +0900

    2022-05-05T06:55:43.118+09:00

---
+++ src/project/Kdwkwgsyar.java
@@ -0,0 +1,13 @@
+package project;
+
+public class Kdwkwgsyar {
+	String message = "Kdwkwgsyar";
+	
+	public Kdwkwgsyar() {
+		super();
+	}
+	
+	public void speak() {
+		System.out.println(message);
+	}
+}
--- src/project/Main.java
+++ src/project/Main.java
@@ -8,6 +8,10 @@ public class Main {
 		System.out.println("HelloWorld");
 		// START
 
+		Kdwkwgsyar kdwkwgsyar = new Kdwkwgsyar();
+		kdwkwgsyar.speak();
+
+
 		Wtxbgowjib wtxbgowjib = new Wtxbgowjib();
 		wtxbgowjib.speak();
 
